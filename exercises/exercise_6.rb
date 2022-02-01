eb2e7f84b5aacd1a84519611e4d592bc327022e7
Brenon <brenonparry@gmail.com>
2022-02-01 04:35:37 +0000

    Created 8 new employees, half in @store1 and half in @store2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# @store1 employees
+@store1.employees.create(first_name: "Lloyd", last_name: "Christmas", hourly_rate: 5.75)
+@store1.employees.create(first_name: "Harry", last_name: "Dunn", hourly_rate: 2.39)
+@store1.employees.create(first_name: "Marry", last_name: "Swanson", hourly_rate: 69)
+@store1.employees.create(first_name: "Brendan", last_name: "Fraser", hourly_rate: 1000)
+
+#  @store2 employees
+@store2.employees.create(first_name: "Steve", last_name: "Rogers", hourly_rate: 120)
+@store2.employees.create(first_name: "James", last_name: "Barns", hourly_rate: 1)
+@store2.employees.create(first_name: "Natashia", last_name: "Rominoff", hourly_rate: 90)
+@store2.employees.create(first_name: "Tony", last_name: "Stark", hourly_rate: 1000)
